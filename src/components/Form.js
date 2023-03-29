e8a355cb1b2c14fc9b87b9ecd4e5e56e1481e518
Bosch <boschio91@gmail.com>
2023-03-29 12:30:41 -0400

    Added form error checking. Form now clears once link is submitted.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,32 @@
 import { useState } from 'react'
 
 const Form = ({ handleSubmit }) => {
-  const [form, setForm] = useState({
-    name: "",
-    URL: ""
-  })
+  const [form, setForm] = useState({name:"",URL:""})
   const handleChange = (event) => {
        setForm((form) => ({ ...form, [event.target.name]: event.target.value }))
   }
 
   const onFormSubmit = (event) => {
     event.preventDefault()
-    handleSubmit(form)
+    
+    let url = form.URL
+    if (form.name === '' || form.url === '' || form === null) {
+      window.alert("\nPlease enter a valid Name or URL:\n\nName field must not be blank. URL must start with 'https://'")
+    } else if (!url.startsWith("http")) {
+      window.alert("\nPlease enter a valid URL:\n\nURL must start with 'https://'")
+    } else {
+      handleSubmit(form)
+      document.getElementById('form').reset()
+      setForm({})
+    }
   }
 
   return (
-    <form onSubmit={onFormSubmit}>
+    <form id='form' onSubmit={onFormSubmit}>
       <label>Name</label>
       <input type='text' name='name' onChange={handleChange}></input>
       <label>URL</label>
-      <input type='url' name='URL' onChange={handleChange}></input>
+      <input type='text' name='URL' onChange={handleChange}></input>
       <button type='submit'>Submit</button>
     </form>
   )
