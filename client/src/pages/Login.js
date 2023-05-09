403c72dc0b2f1a007cab8a4df26cf7632468f467
Boschio <boschio91@gmail.com>
2023-05-09 09:38:23 -0400

    Changed login form to use new handlers. Still WIP.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,42 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Login() {
+  const [loginForm, setLoginForm] = useState({
+    username: "",
+    password: "",
+  })
+
+  const handleInputChange = (event) => {
+    setLoginForm((form) => ({ ...form, [event.target.name]: event.target.value }))
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+    
+    if (loginForm.username === '' || loginForm.password === '' || loginForm === null) {
+      window.alert("\nPlease enter a valid username or password:\n\nFields must not be blank.")
+    } else {
+      handleOnSubmit(loginForm)
+    }
+  }
+
+  const handleOnSubmit = async (event) => {
+    // Should implement jwt and user context? Will also need to add relation in DB for specific user's links
+    await fetch(`/auth/login`, { method: 'POST', headers: { 'Content-Type': 'application/json' },body: JSON.stringify({ username: loginForm.username, password:loginForm.password })})
+  }
+
   return (
     <div className="Login">
       <h1>Sign in</h1>
-        <form action="/auth/login" method="post">
+        <form onSubmit={onFormSubmit} >
             <section>
                 <label for="username">Username</label>
-                <input id="username" name="username" type="text" autocomplete="username" required autofocus></input>
+                <input id="username" name="username" type="text" autocomplete="username" onChange={handleInputChange} required autofocus></input>
             </section>
             <section>
                 <label for="current-password">Password</label>
-                <input id="current-password" name="password" type="password" autocomplete="current-password" required></input>
+                <input id="current-password" name="password" type="password" autocomplete="current-password" onChange={handleInputChange} required></input>
             </section>
             <button type="submit">Sign in</button>
         </form>
